918016bfd9ea62ef579e5c1f80e108e9b7405ddf
JumpLikeFLEA <chernov.gleb23@gmail.com>
2026-04-29 21:27:11 +0200

    fix strategies

--- strategies/__init__.py
+++
@@ -1,5 +0,0 @@
-from .ma_crossover_strategy import MACrossoverStrategy
-from .ma_trend_strategy import MATrendStrategy
-from .rsi_strategy import RSIStrategy
-
-__all__ = ["MACrossoverStrategy", "MATrendStrategy", "RSIStrategy"]
